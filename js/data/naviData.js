0acd8512cd09f96d1633ab31d847061cd61f44a8
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2021-01-09 16:00:57 +0200

    JS Navigation

---
+++ js/data/naviData.js
@@ -0,0 +1,34 @@
+const naviData = {
+    selector: 'nav',
+    structure: [
+        {
+        title: 'Home',
+        href:  '#'
+    },
+    {
+        title: 'Company',
+        href:  '#'
+    },
+    {
+        title: 'IT Solutions',
+        href:  '#'
+    },
+    {
+        title: 'Elements',
+        href:  '#'
+    },
+    {
+        title: 'Case Studies',
+        href:  '#'
+    },
+    {
+        title: 'Blog',
+        href:  '#'
+    },
+]
+}
+
+
+
+
+export { naviData }
--- js/home-main.js
+++ js/home-main.js
@@ -1,9 +1,17 @@
 // ALL IMPORT
 // header
+import { NaviBar } from './components/navBar/NaviBar.js';
+import { naviData} from './data/naviData.js';
 // hero
 // services
 
 // CODE EXECUTION
 // header
+const naviBar = new NaviBar(naviData);
+naviBar.init();
+console.log(naviBar);
+
+
 // hero
 // services
+
