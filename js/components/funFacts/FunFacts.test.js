f85adfa2689114b339fff03faf6c76b31e5488be
Roberta2020 <74533119+Roberta2020@users.noreply.github.com>
2021-01-12 16:07:03 +0200

    added several tests

--- js/components/funFacts/FunFacts.test.js
+++ js/components/funFacts/FunFacts.test.js
@@ -5,4 +5,25 @@ describe('Tikrinami parametrai', () => {
         const funFacts = new FunFacts();
         expect(funFacts.init()).toBeFalsy();
     })
+    test('Nevalidu kai duodamas skaicius', () => {
+        const funFacts = new FunFacts();
+        expect(funFacts.init(240)).toBeFalsy();
+    })
+    test('Nevalidu kai duodama logine reiksme "true"', () => {
+        const funFacts = new FunFacts();
+        expect(funFacts.init(true)).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas logine reiksme "false"', () => {
+        const funFacts = new FunFacts();
+        expect(funFacts.init(false)).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas tekstas', () => {
+        const funFacts = new FunFacts();
+        expect(funFacts.init('gvgvhvh')).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas tuscias tekstas', () => {
+        const funFacts = new FunFacts();
+        expect(funFacts.init('')).toBeFalsy();
+    })
 })
+
--- js/components/funFacts/isValidFunFactsItem.test.js
+++ js/components/funFacts/isValidFunFactsItem.test.js
@@ -6,4 +6,24 @@ describe('Tikrinami parametrai', () => {
         const funFacts = new FunFacts();
         expect(isValidFunFactsItem()).toBeFalsy();
     })
+    test('Nevalidu kai duodamas skaicius', () => {
+        const funFacts = new FunFacts();
+        expect(isValidFunFactsItem(562)).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas sarasas', () => {
+        const funFacts = new FunFacts();
+        expect(isValidFunFactsItem([])).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas boolean', () => {
+        const funFacts = new FunFacts();
+        expect(isValidFunFactsItem(true)).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas boolean', () => {
+        const funFacts = new FunFacts();
+        expect(isValidFunFactsItem(false)).toBeFalsy();
+    })
+    test('Nevalidu kai duodamas tuscias objektas', () => {
+        const funFacts = new FunFacts();
+        expect(isValidFunFactsItem({})).toBeFalsy();
+    })
 })
